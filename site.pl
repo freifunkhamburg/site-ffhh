363a98ece30e54c95f29d09ca3d5b3d29524d2c9
Matthias Schiffer <mschiffer@universe-factory.net>
2013-09-28 18:45:03 +0200

    Add ffhl wifi config

---
+++ site.pl
@@ -0,0 +1,14 @@
+{
+    wifi24 => {
+	ssid => 'luebeck.freifunk.net',
+	channel => 1,
+	mesh_ssid => => '02:d1:11:37:fc:38',
+	mesh_bssid => => '02:d1:11:37:fc:38',
+    },
+    wifi5 => {
+	ESSID => 'luebeck.freifunk.net (5GHz)',
+	channel => 44,
+	mesh_ssid => => '02:d1:11:37:fc:38',
+	mesh_bssid => => '02:d1:11:37:fc:38',
+    },
+};
