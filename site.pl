4a3122686778d6e7640d385f9f48398f3b567bb1
Matthias Schiffer <mschiffer@universe-factory.net>
2013-09-28 19:38:32 +0200

    Fix wifi5 ssid

--- site.pl
+++ site.pl
@@ -6,9 +6,9 @@
 	mesh_bssid => => '02:d1:11:37:fc:38',
     },
     wifi5 => {
-	ESSID => 'luebeck.freifunk.net (5GHz)',
+	ssid => 'luebeck.freifunk.net (5GHz)',
 	channel => 44,
 	mesh_ssid => => '02:d1:11:37:fc:38',
 	mesh_bssid => => '02:d1:11:37:fc:38',
     },
-};
+}
