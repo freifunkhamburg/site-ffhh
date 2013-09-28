013044c5d3b7e86cd0b3ad8acbfc197eb6d022ca
Matthias Schiffer <mschiffer@universe-factory.net>
2013-09-28 21:25:10 +0200

    Config for the next-node anycast address

--- site.mk
+++ site.mk
@@ -1,2 +1,2 @@
-GLUON_SITE_PACKAGES := gluon-config-mode gluon-mesh-batman-adv gluon-mesh-vpn-fastd \
+GLUON_SITE_PACKAGES := gluon-config-mode gluon-next-node gluon-mesh-batman-adv gluon-mesh-vpn-fastd \
 	htop iputils-ping6
--- site.pl
+++ site.pl
@@ -11,4 +11,14 @@
 	mesh_ssid => => '02:d1:11:37:fc:38',
 	mesh_bssid => => '02:d1:11:37:fc:38',
     },
+
+    next_node => {
+	ip4 => '10.130.0.1',
+	prefix4 => '10.130.0.0/20',
+
+	ip6 => 'fdef:ffc0:3dd7::1',
+	prefix6 => 'fdef:ffc0:3dd7::/64',
+
+	mac => '16:41:95:40:f7:dc',
+    },
 }
