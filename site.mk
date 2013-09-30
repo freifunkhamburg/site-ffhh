99e43524b8999e34771e0a736affead4ae686977
Matthias Schiffer <mschiffer@universe-factory.net>
2013-09-30 21:25:38 +0200

    Add backbone config

--- site.mk
+++ site.mk
@@ -1,2 +1,2 @@
 GLUON_SITE_PACKAGES := gluon-config-mode gluon-cron gluon-ebtables gluon-next-node gluon-mesh-batman-adv gluon-mesh-vpn-fastd gluon-autoupdater \
-	htop iputils-ping6 iptables kmod-ipt-nathelper firewall
+	htop iputils-ping6 iptables kmod-ipt-nathelper firewall fastd haveged
