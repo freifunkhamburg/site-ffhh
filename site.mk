3b7314ee19cb3c0f105f5fe40f75b8985693a355
Nils Schneider <nils@nilsschneider.net>
2013-09-30 22:40:03 +0200

    site.mk: one package per line

--- site.mk
+++ site.mk
@@ -1,2 +1,15 @@
-GLUON_SITE_PACKAGES := gluon-config-mode gluon-cron gluon-ebtables gluon-next-node gluon-mesh-batman-adv gluon-mesh-vpn-fastd gluon-autoupdater \
-	htop iputils-ping6 iptables kmod-ipt-nathelper firewall fastd haveged
+GLUON_SITE_PACKAGES := \
+	gluon-config-mode \
+	gluon-cron \
+	gluon-ebtables \
+	gluon-next-node \
+	gluon-mesh-batman-adv \
+	gluon-mesh-vpn-fastd \
+	gluon-autoupdater \
+	htop \
+	iputils-ping6 \
+	iptables \
+	kmod-ipt-nathelper \
+	firewall \
+	fastd \
+	haveged
