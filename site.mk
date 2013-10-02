1a50332201d8445d7d8ea57295eb9a7b96dd8161
Matthias Schiffer <mschiffer@universe-factory.net>
2013-10-02 20:38:05 +0200

    Remove packages installed and enabled as dependencies now

--- site.mk
+++ site.mk
@@ -1,7 +1,5 @@
 GLUON_SITE_PACKAGES := \
 	gluon-config-mode \
-	gluon-cron \
-	gluon-ebtables \
 	gluon-next-node \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
@@ -12,5 +10,4 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	kmod-ipt-nathelper \
 	firewall \
-	fastd \
 	haveged
