b6c64db8ecc4d2ebf784712393e6a061aacaffd2
Matthias Schiffer <mschiffer@universe-factory.net>
2014-06-06 13:02:17 +0200

    Add gluon-firewall package

--- site.mk
+++ site.mk
@@ -4,6 +4,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-firewall \
 	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
