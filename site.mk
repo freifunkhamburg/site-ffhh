d5a09c3b3642aef278e6e0e384e95b52e984bd0e
ohrensessel <git@ohrensessel.net>
2014-06-25 06:56:56 +0200

    Revert "Merge branch 'master' of https://github.com/freifunk-gluon/site-ffhl"
    This reverts commit 01733c25e77d839d626c2fa31faefd43dff1269c, reversing
    changes made to 0a2d554ac630f0d5edc4e62630da837fe83bece7.

--- site.mk
+++ site.mk
@@ -18,7 +18,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.4.1+0-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
