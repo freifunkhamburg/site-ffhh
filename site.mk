91df0a803663bf235d27d7339537ab09e6a6cbe4
ohrensessel <git@ohrensessel.net>
2014-06-26 23:29:01 +0200

    Use ffhh-luci-admin instead of gluon-luci-admin
    to resolve dependency for gluon-config-mode

--- site.mk
+++ site.mk
@@ -6,7 +6,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-firewall \
 	gluon-legacy \
-	gluon-luci-admin \
+	ffhh-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-next-node \
 	gluon-mesh-batman-adv \
