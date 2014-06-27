a8d8fcd2b2a5d58e8a1587950a2f348fc8fe9269
ohrensessel <git@ohrensessel.net>
2014-06-27 23:42:57 +0200

    New experimental release numbers

--- site.mk
+++ site.mk
@@ -18,7 +18,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.4.2
+DEFAULT_GLUON_RELEASE := 0.4.2+0-exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
