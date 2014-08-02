c2a37cabfd8edba29fb6aca07a35b1990506c675
Matthias Schiffer <mschiffer@universe-factory.net>
2014-08-02 22:20:25 +0200

    New experimental releases

--- site.mk
+++ site.mk
@@ -19,7 +19,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.5
+DEFAULT_GLUON_RELEASE := 0.5+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
