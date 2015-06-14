6a73a2e1ce292c25de88e0766d72ab273425f6d2
bdobe <bdobe@users.noreply.github.com>
2015-06-14 18:55:56 +0200

    Remove support for upgrading legacy nodes
    Directly upgrade from pre-gluon notes is no longer supported.
    
    This commit also cleans some white spaces.

--- site.mk
+++ site.mk
@@ -4,13 +4,12 @@ GLUON_SITE_PACKAGES := \
 	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
-        gluon-config-mode-core \
+	gluon-config-mode-core \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-reboot-ffhh \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
@@ -18,7 +17,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
-        gluon-setup-mode \
+	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \
