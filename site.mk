2c4d21b3663f60126c8249ff72133b888a3a474d
bdobe <bdobe@users.noreply.github.com>
2015-10-03 17:24:21 +0200

    Update Branch v0.7.x

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.7
+DEFAULT_GLUON_RELEASE := 0.7+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
