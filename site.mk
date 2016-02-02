3eb2c7f3d3aadcf913d7d798d63f152e40ef2a76
Matthias Schiffer <mschiffer@universe-factory.net>
2016-02-02 11:18:34 +0100

    Adjust to gluon-announced -> gluon-respondd rename

--- site.mk
+++ site.mk
@@ -1,7 +1,6 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
@@ -19,6 +18,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-respondd \
 	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
