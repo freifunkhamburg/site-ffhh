766b9e70d50086b310fb77e7c8155a0749c9816b
bdobe <bdobe@users.noreply.github.com>
2016-02-02 19:25:28 +0100

    site.mk: replace package gluon-announced with gluon-respondd

--- site.mk
+++ site.mk
@@ -1,7 +1,6 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-core \
@@ -18,6 +17,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-respondd \
 	gluon-setup-mode \
 	gluon-status-page \
 	fastd-traffic-status \
