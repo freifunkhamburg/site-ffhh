f427c1023a35943e3c93552b1cb75369f279836b
Daniel Frank <github-523@danielfrank.net>
2017-10-06 21:00:37 +0200

    0.8.4 experimental -> stable

--- sites/ffhh-sued/site.mk
+++ sites/ffhh-sued/site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.8.4+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.8.4
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
