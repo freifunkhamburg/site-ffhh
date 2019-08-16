44e04d3d9c7f7a435e7740aad3c0d023a54c2079
Daniel Frank <git@danielfrank.net>
2019-08-16 23:01:45 +0200

    build.sh: dont build everything while detecting the targets...

--- build.sh
+++ build.sh
@@ -96,7 +96,8 @@ done
 
 if [ "$auto_targets" = "1" ]; then
 	# detect available targets
-	targets="$(make | tee /dev/stderr | awk '$1 == "*" {print $2}' | sort | xargs)"
+	make
+	targets="$(make | awk '$1 == "*" {print $2}' | sort | xargs)"
 else
 	# if a list of build targets has been supplied, only build those
 	targets="$(echo "${build_targets:-$targets}" | sed -e 's_,_ _g')"
