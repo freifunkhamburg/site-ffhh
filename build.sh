74088a18a9c2cc648bcd341d7768e8f6bc4c7d71
Daniel Frank <git@danielfrank.net>
2019-08-18 23:55:55 +0200

    build.sh: fix remove known-broken images after the build

--- build.sh
+++ build.sh
@@ -112,7 +112,7 @@ done
 # shellcheck disable=SC2154
 for broken_image in "${broken_images[@]}"; do
 	announce "Removing broken image ${broken_image}..."
-	find "${GLUON_OUTPUTDIR}/${GLUON_BRANCH}/images" -iname "${broken_image}" \( -type f -o -type l \) -ls -exec rm -f {} \;
+	find "${GLUON_OUTPUTDIR}/images" -iname "${broken_image}" \( -type f -o -type l \) -ls -exec rm -f {} \;
 done
 # Generate the images.list
 # shellcheck disable=SC2094
